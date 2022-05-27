d92de5f37ee13513c4d5a1694482018120553108
Android dev <i.kurak@complitech.org>
2022-05-27 16:51:15 +0300

    Add new screens for work test

---
+++ app/src/main/java/com/io/myutilsproject/screens/sixth/SixthScreen.kt
@@ -0,0 +1,21 @@
+package com.io.myutilsproject.screens.sixth
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import com.io.myutilsproject.screens.third.ThirdState
+
+@Composable
+fun SixthScreen(
+    state: ThirdState,
+    inc: () -> Unit,
+){
+    Column() {
+        Text("state ${state.count}")
+        Button(onClick = { inc() }) {
+            
+        }
+    }
+    
+}
