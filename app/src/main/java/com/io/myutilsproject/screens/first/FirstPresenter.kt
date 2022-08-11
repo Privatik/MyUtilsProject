2e3af23136e66117cbb3e4da97d4848143e53689
Android dev <i.kurak@complitech.org>
2022-08-11 11:44:16 +0300

    Change order call machine and fix memory leak

--- app/src/main/java/com/io/myutilsproject/screens/first/FirstPresenter.kt
+++ app/src/main/java/com/io/myutilsproject/screens/first/FirstPresenter.kt
@@ -2,16 +2,15 @@ package com.io.myutilsproject.screens.first
 
 import com.example.machine.ReducerDSL
 import com.io.myutilsproject.Presenter
-import com.io.myutilsproject.repository.FirstRepository
 import com.io.myutilsproject.repository.SecondRepository
 import javax.inject.Inject
 
 class FirstPresenter @Inject constructor(
-    private val firstRepository: FirstRepository,
     private val secondRepository: SecondRepository,
-): Presenter<Any, Any, Any>(Any()) {
+): Presenter<Any, Any, Any>(Pair(Any()) {}) {
 
-    override fun machine(): ReducerDSL<Any, Any>.() -> Unit = {
+    override fun ReducerDSL<Any, Any>.machine() {
 
     }
+
 }
